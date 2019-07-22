6b0178712772d281dd7ab582e928e1dd0799c738
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-22 20:35:09 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -20,9 +20,8 @@ def third_challenge
         #your key/value pairs here
         },
         
-      :heroine => { name: "Juliet", age: "15" , status: "alive" },
-        #your key/value pairs here
-      :heroine_friends => []
+       :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
    }
   }
 
